a40e44e82e97cdabf9498731712344c28605f369
Lorys Lellouche <lorys.lellouche@gmail.com>
2026-03-14 18:09:40 -0400

    adding create

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -23,39 +23,41 @@ public class Demo {
                  */
 
 
-                String sql = "insert into student values (8, 'Johninhohff', '49')";
-                boolean status = st.execute(sql);
-
-                System.out.println(status);
+                String createSql = "insert into new_student (name, age) values (?,?)";
+                PreparedStatement ppst = connection.prepareStatement(createSql);
+                ppst.setString(1,"Navin");
+                ppst.setInt(2,45);
+                int rowsAffected = ppst.executeUpdate();
+                System.out.println("rows affected: " + rowsAffected);
 
 
 
                 /*
                 read
                  */
-//                String sql = "select * from student";
-//                ResultSet rs = st.executeQuery(sql);
-//                ResultSetMetaData rsmd = rs.getMetaData();
-//                int columnCount = rsmd.getColumnCount();
-//                System.out.println("column count: " + columnCount);
-//
-//
-//                while (rs.next()) {
-//                    System.out.println("------ nouvelle ligne -----");
-//
-//                    for (int i = 1; i < columnCount + 1; i++) {
-//                        String columnName = rsmd.getColumnName(i);
-//                        int type = rsmd.getColumnType(i);
-//
-//                        if (type == Types.INTEGER || type == Types.BIGINT) {
-//                            System.out.println(columnName + ": " + rs.getInt(i));
-//                        } else if (type == Types.VARCHAR || type == Types.CHAR) {
-//                            System.out.println(columnName + ": " + rs.getString(i));
-//                        } else {
-//                            System.out.println(columnName + ": " + rs.getObject(i));
-//                        }
-//                    }
-//                }
+                String readSql = "select * from new_student";
+                ResultSet rs = st.executeQuery(readSql);
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+                System.out.println("column count: " + columnCount);
+
+
+                while (rs.next()) {
+                    System.out.println("------ nouvelle ligne -----");
+
+                    for (int i = 1; i < columnCount + 1; i++) {
+                        String columnName = rsmd.getColumnName(i);
+                        int type = rsmd.getColumnType(i);
+
+                        if (type == Types.INTEGER || type == Types.BIGINT) {
+                            System.out.println(columnName + ": " + rs.getInt(i));
+                        } else if (type == Types.VARCHAR || type == Types.CHAR) {
+                            System.out.println(columnName + ": " + rs.getString(i));
+                        } else {
+                            System.out.println(columnName + ": " + rs.getObject(i));
+                        }
+                    }
+                }
 
                 /*
                 update
